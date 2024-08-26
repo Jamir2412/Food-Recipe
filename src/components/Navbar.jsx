01d9d7af375b0afa028595046ef8593f9f473a57
Jamir Ahmed <jamir.ahmed.ghtwtrlkdn@gmail.com>
2024-08-26 06:58:52 +0530

    Change letter case

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import styles from "../Styles/navbar.module.css";
+import styles from "../styles/navbar.module.css";
 export default function Navbar(){
     return (
         <div className={styles.nav}>🍔 A-1 Family Restraunt</div>
